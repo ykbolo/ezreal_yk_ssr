edde070529adfabbc6cf41fb900c53c1b9c9babe
ykbolo <yk84732225!!>
2021-05-17 10:43:02 +0800

    fix: 金宝的图片

--- pages/tech/index.vue
+++ pages/tech/index.vue
@@ -2,9 +2,9 @@
  * @Author: Yang Kang
  * @Date: 2021-05-13 14:20:06
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-05-13 14:22:07
+ * @LastEditTime: 2021-05-17 10:31:43
 -->
 <template>
-  <div></div>
+  <div class="container"></div>
 </template>
 <script src="./component.js"></script>
