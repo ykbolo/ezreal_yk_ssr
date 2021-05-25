9496e2c8506b07762d75f5cf6b78e37a0da6f256
ykbolo <503589510@qq.com>
2021-05-25 17:37:11 +0800

    fix: 修复路径

--- server/apis/uploadImage.js
+++ server/apis/uploadImage.js
@@ -2,7 +2,7 @@
  * @Author: Yang Kang
  * @Date: 2021-05-06 15:22:01
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-05-25 16:21:43
+ * @LastEditTime: 2021-05-25 17:36:29
  */
 
 const express = require('express')
@@ -10,6 +10,8 @@ const router = express.Router()
 const multer = require('multer')
 const md5 = require('md5-node')
 // var upload = multer({ dest: './public/img' }).any()
+let filename = ''
+
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
     //    指定文件存放路径
@@ -21,7 +23,7 @@ let storage = multer.diskStorage({
     let exts = file.originalname.split('.')
     let ext = exts[exts.length - 1] //为了防止上传图片时,图片的名称中含多个点,从后面取最后一个解决问题
     let tmpname = Date.now() + parseInt(Math.random() * 9999) //时间戳+随机数生成文件名
-    // tmpname = md5(tmpname)
+    filename = `${md5(tmpname)}.${ext}`
     cb(null, `${md5(tmpname)}.${ext}`)
   }
 })
@@ -30,8 +32,8 @@ let upload = multer({ storage: storage })
 router.post('/uploadImage', upload.any(), (req, res) => {
   console.log(req.files)
   let file = req.files?.[0]
-  let fis = file.path.split('\\')
-  let filename = fis[fis.length - 1]
+  // let fis = file.path.split('\\')
+  // let filename = fis[fis.length - 1]
   res.json({
     code: '0000',
     type: 'single',
