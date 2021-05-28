a7c1d1eca8106b71bd587f3b64bd70457ae20576
ykbolo <503589510@qq.com>
2021-05-28 18:21:42 +0800

    feat: 增加百度抓取标记

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'baidu-site-verification', content: 'code-av5DMXR48f' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
