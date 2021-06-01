4fbe52140a6a6bf60d5a2a358104b089cee89233
ykbolo <503589510@qq.com>
2021-06-01 10:49:00 +0800

    fix: page-url

--- pages/list/_type.vue
+++ pages/list/_type.vue
@@ -2,13 +2,13 @@
  * @Author: Yang Kang
  * @Date: 2021-05-18 16:16:21
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-05-24 17:56:36
+ * @LastEditTime: 2021-06-01 10:48:13
 -->
 
 <template>
   <div class="container">
     <div v-for="(item, index) in items" :key="item.md5" class="list-item p-20 b-radius-8 m-t-15">
-      <a class="list-title f-20 f-blue" :href="'/detail/' + item.md5" target="_blank">{{ index + (current - 1) * 5 + 1 }}. {{ item.title }}</a>
+      <a class="list-title f-20 f-blue" :href="'/detail/' + item.md5" target="_blank">{{ index + (page - 1) * 5 + 1 }}. {{ item.title }}</a>
       <div class="row m-t-10">
         <div class="col-6">{{ item.time }}</div>
       </div>
@@ -17,7 +17,7 @@
       </div>
     </div>
     <div class="t-right m-t-30 p-b-30">
-      <a-pagination v-model="current" :total="total" show-less-items :pageSize="5" :defaultCurrent="1" @change="onPageChange" />
+      <a-pagination v-model="page" :total="total" show-less-items :pageSize="5" :defaultCurrent="1" @change="onPageChange" />
     </div>
   </div>
 </template>
@@ -28,28 +28,26 @@
       return true
     },
     layout: 'blog',
-    async asyncData({ params }) {
-      let md5 = params.md5
-      const result = await service.getMdFromMysql({ start: 0, hit: 5 })
+    async asyncData({ query }) {
+      const page = +query.page || 1
+      const hit = 5
+      const result = await service.getMdFromMysql({ start: (page - 1) * hit, hit })
       return {
-        md5: params.md5,
+        page: page,
         items: result.items,
         total: result.total
       }
     },
     data() {
-      return {
-        current: 1
-      }
-    },
-    mounted() {
-      console.log(this.items)
+      return {}
     },
     methods: {
       async onPageChange(page, pageSize) {
-        console.log(page, pageSize)
-        const result = await service.getMdFromMysql({ start: (page - 1) * pageSize, hit: 5 })
+        let newPage = window.location.href.replace(/page=\d+/, `page=${page}`)
+        history.pushState('', '', newPage)
 
+        const result = await service.getMdFromMysql({ start: (page - 1) * pageSize, hit: 5 })
+        this.page = page
         this.items = result.items
         this.total = result.total
       }
