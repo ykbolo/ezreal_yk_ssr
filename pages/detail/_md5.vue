9dfabad4161cc9b98c9da122853bd90da7953e50
ykbolo <503589510@qq.com>
2021-06-03 12:02:29 +0800

    左右导航

--- pages/detail/_md5.vue
+++ pages/detail/_md5.vue
@@ -2,12 +2,27 @@
  * @Author: Yang Kang
  * @Date: 2021-05-18 16:16:21
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-06-01 11:09:59
+ * @LastEditTime: 2021-06-03 12:02:12
 -->
 
 <template>
   <div class="container">
     <div id="md" class="container" v-html="marked(content)"></div>
+    <div class="row p-15">
+      <div class="col-4">
+        <nuxt-link class="pre d-flex" :to="`/detail/${pre.md5}`">
+          <img src="./images/left.svg" alt="" width="16px" />
+          {{ pre.title }}</nuxt-link
+        >
+      </div>
+      <div class="col-2"></div>
+      <div class="col-2"></div>
+      <div class="col-4 t-right">
+        <nuxt-link class="next d-flex justify-content-end" :to="`/detail/${next.md5}`">
+          {{ next.title }} <img src="./images/right.svg" alt="" width="16px"
+        /></nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -36,6 +51,8 @@
       let md5 = params.md5
       const result = await service.getArticleByMd5({ md5 })
       return {
+        pre: result.preNext?.pre || {},
+        next: result.preNext?.next || {},
         title: result.title,
         md5: params.md5,
         content: result.content
--- server/apis/getArticleByMd5.js
+++ server/apis/getArticleByMd5.js
@@ -2,9 +2,9 @@
  * @Author: Yang Kang
  * @Date: 2021-05-06 15:22:01
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-05-24 14:21:44
+ * @LastEditTime: 2021-06-03 11:49:20
  */
-import { Router } from 'express'
+import e, { Router } from 'express'
 import mysql from 'mysql'
 import config from '../../config/mysql'
 
@@ -34,22 +34,51 @@ const queryItems = params => {
     )
   })
 }
-function getMdFromMysql(params, res) {
-  Promise.all([queryItems(params)]).then(([result]) => {
-    if (result && result.length) {
-      let detail = result[0]
-      res.send(detail)
-    } else {
-      res.send({})
-    }
+const queryPreNext = id => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      // `select * from tb_articles_for_life`,
+      `select * from tb_techs where id=${id - 1} or id=${id + 1}`,
+      (err, result) => {
+        if (err) {
+          throw err
+        }
+        let preNext = {}
+        if (result && result.length === 1) {
+          if (id === 1) {
+            preNext.next = { title: result[0].title, md5: result[0].md5 }
+          } else {
+            preNext.pre = { title: result[0].title, md5: result[0].md5 }
+          }
+        } else if (result && result.length === 2) {
+          preNext.pre = { title: result[0].title, md5: result[0].md5 }
+          preNext.next = { title: result[1].title, md5: result[1].md5 }
+        }
+        resolve(preNext)
+      }
+    )
   })
+}
+function getMdFromMysql(params, res) {
+  queryItems(params).then(item => {
+    let detail = item[0]
+    queryPreNext(detail.id).then(preNext => {
+      detail.preNext = preNext
+      res.send(detail)
+    })
+  })
+  // Promise.all([queryItems(params)]).then(([result]) => {
+  //   if (result && result.length) {
+  //     let detail = result[0]
+  //     console.log(detail)
+  //     res.send(detail)
+  //   } else {
+  //     res.send({})
+  //   }
+  // })
 }
 
 router.post('/getArticleByMd5', function (req, res, next) {
-  console.log(req.body, 11)
-  console.log(req.query, 11)
-  console.log(req.params, 11)
-  // console.log(req)
   getMdFromMysql(req.body, res)
 })
 export default router
