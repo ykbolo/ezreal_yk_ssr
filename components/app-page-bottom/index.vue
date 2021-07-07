2182fb162ac7adcb2f134ad04e0804532b87575e
ykbolo <503589510@qq.com>
2021-07-07 14:47:40 +0800

    feat: 增加网站底部备案链接

--- components/app-page-bottom/index.vue
+++ components/app-page-bottom/index.vue
@@ -2,12 +2,14 @@
  * @Author: Yang Kang
  * @Date: 2021-05-06 11:53:59
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-05-28 16:33:31
+ * @LastEditTime: 2021-07-07 14:47:12
 -->
 <template>
   <div class="app-page-bottom p-30 b-gray">
     <div class="container f-white">
-      <p class="f-16">备案号：苏ICP备19075717号-1</p>
+      <p class="f-16">
+        <a href="https://beian.miit.gov.cn/" target="_blank"> 备案号：苏ICP备19075717号-1 </a>
+      </p>
       <p class="f-14">邮箱：503589510@qq.com</p>
       <p class="f-14">电话：17351368414</p>
     </div>
