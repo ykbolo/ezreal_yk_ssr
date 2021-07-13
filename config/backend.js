e86ec5ad9794738c9cc7571a1846259672124610
ykbolo <503589510@qq.com>
2021-07-13 10:57:52 +0800

    fix: 恢复前后端分离模式

--- config/backend.js
+++ config/backend.js
@@ -2,8 +2,8 @@ let config = {
   host: ''
 }
 if (__DEV__) {
-  config.host = 'http://127.0.0.1:8080/backend'
+  config.host = 'http://127.0.0.1:9003'
 } else if (__PROD__) {
-  config.host = 'http://112.124.56.144:8080/backend'
+  config.host = 'http://112.124.56.144:9003'
 }
 export default config
--- nuxt.config.js
+++ nuxt.config.js
@@ -61,10 +61,10 @@ export default {
     // https://go.nuxtjs.dev/eslint
     // '@nuxtjs/eslint-module',
   ],
-  serverMiddleware: [
-    // API middleware
-    '~/server-middleware/server-ts/index.ts'
-  ],
+  // serverMiddleware: [
+  //   // API middleware
+  //   '~/server-middleware/server-ts/index.ts'
+  // ],
   // Modules: https://go.nuxtjs.dev/config-modules
   // modules: ['@nuxtjs/axios', '@nuxtjs/proxy'],
 
