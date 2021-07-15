27010f5ff0aef3d63068b171e41e48a8386b9729
ykbolo <503589510@qq.com>
2021-07-15 11:48:23 +0800

    fix: dev和线上分库

--- config/mysql/index.js
+++ config/mysql/index.js
@@ -2,11 +2,22 @@
  * @Author: Yang Kang
  * @Date: 2021-05-12 16:03:21
  * @LastEditors: Yang Kang
- * @LastEditTime: 2021-05-12 16:13:03
+ * @LastEditTime: 2021-07-15 11:48:02
  */
-module.exports = {
-  host: 'rm-bp11694i5wj393ld5oo.mysql.rds.aliyuncs.com',
-  user: 'yangkang',
-  database: 'db_ezreal_yk_cn',
-  password: 'Yk84732225!!',
+let config
+if (__DEV__) {
+  config = {
+    host: 'rm-bp11694i5wj393ld5oo.mysql.rds.aliyuncs.com',
+    user: 'yangkang',
+    database: 'db_ezreal_yk_cn',
+    password: 'Yk84732225!!'
+  }
+} else {
+  config = {
+    host: 'rm-bp11694i5wj393ld5oo.mysql.rds.aliyuncs.com',
+    user: 'yangkang',
+    database: 'db_ezreal_yk_cn_prod',
+    password: 'Yk84732225!!'
+  }
 }
+export default config
